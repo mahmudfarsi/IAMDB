256575854067311c206e939f6c8c8706e723775f
mahmud farsi <mhfarsi002022@gmail.com>
2024-03-17 15:41:42 +0330

    create page detail-movies

--- src/router/router.js
+++ src/router/router.js
@@ -9,17 +9,14 @@ export default  createRouter({
       component: import('@/pages/HomeView.vue')
     },
     {
-      name:'404',
-      path:'/:patchMatch(.*)*',
-      component:import('@/pages/404.vue')
+      name:'detail',
+      path:'/detail',
+      component:import('@/pages/DetailMovies.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/pages/AboutView.vue')
+      name:'404',
+      path:'/:pathMatch(.*)*',
+      component:import('@/pages/404.vue')
     }
   ]
 })
