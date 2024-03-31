911f2d0c74910498578dd7c2bc52eae56c9afdf2
mahmud farsi <mhfarsi002022@gmail.com>
2024-03-31 19:23:09 +0330

    add style from CardImdb to tailwind.css

--- src/components/main/CardImdb.vue
+++ src/components/main/CardImdb.vue
@@ -1,23 +1,31 @@
 <template>
-  <div class="box-left smm:hidden md:flex md:flex-col">
-    <Img :src="details.Poster" :alt="details.Title" class="rounded-curve-12" loading="lazy"/>
-    <div class="indicator-box flex items-center gap-[30px] mt-[20px] px-[15px]">
-    <div
-      class="radial-progress text-red"
-      :style="[`--value:${numRate}`]"
-      role="progressbar"
-    >
-      {{ details.imdbRating }}
+  <component :is="tag" class="card-imdb-main">
+    <Img
+      :src="details.Poster"
+      :alt="details.Title"
+      class="rounded-curve-12"
+      loading="lazy"
+    />
+    <div class="indicator-box">
+      <div
+        class="radial-progress text-red"
+        :style="[`--value:${numRate}`]"
+        role="progressbar"
+      >
+        <p class="imdb-rates">
+          {{ details.imdbRating }}
+        </p>
+      </div>
+      <div class="flex flex-col">
+        <span class="imdb-votes">
+          {{ details.imdbVotes }}
+        </span>
+        <small class="text-rating">
+          ratings on IMDB
+        </small>
+      </div>
     </div>
-    <div class="flex flex-col">
-      <span class="text-white-iamdb-1 font-bold text-2xsm"> {{ details.imdbVotes }} </span>
-      <small class="font-roboto font-normal text-xsm text-white-iamdb-1">
-        ratings on IMDB
-      </small>
-    </div>
-  </div>
-  </div>
-  
+  </component>
 </template>
 
 <script setup>
@@ -27,17 +35,16 @@ import { shape, string } from "vue-types";
 import Img from "@/components/base/Img.vue";
 
 const props = defineProps({
+  tag:string().def('div'),
   details: shape({
     Poster: string(),
     imdbRating: string(),
     imdbVotes: string(),
-    Title:string()
+    Title: string(),
   }),
 });
 
 const numRate = computed(() => {
-  return Number(props.details?.imdbRating) * 10
-})
-
-
+  return Number(props.details?.imdbRating) * 10;
+});
 </script>
