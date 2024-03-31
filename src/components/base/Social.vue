f38751249efdb340206eed25840d5fd2f3b03e10
mahmud farsi <mhfarsi002022@gmail.com>
2024-04-01 01:22:33 +0330

    edit social and set links

--- src/components/base/Social.vue
+++ src/components/base/Social.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="box-social">
-        <Button class=" box-social--btn" tag="router-link" icon="youtube" to="youtube.com"/>
-        <Button class=" box-social--btn" tag="router-link" icon="facebook" to="youtube.com"/>
-        <Button class=" box-social--btn" tag="router-link" icon="twitter" to="youtube.com"/>
-        <Button class=" box-social--btn" tag="router-link" icon="pintrest" to="youtube.com"/>
+        <Button class=" box-social--btn" tag="a" icon="youtube" href="https://www.youtube.com/"/>
+        <Button class=" box-social--btn" tag="a" icon="facebook" href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2F"/>
+        <Button class=" box-social--btn" tag="a" icon="twitter" href="https://twitter.com/?lang=en"/>
+        <Button class=" box-social--btn" tag="a" icon="pintrest" href="https://www.pinterest.com/"/>
     </div>
 </template>
 
